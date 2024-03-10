de97d09b909fe3ad97e180d4849f8766c1100511
Gcehao <162271455+Gcehao@users.noreply.github.com>
2024-03-10 11:57:27 +0800

    Update Dockernginix.sh

--- Dockernginix.sh
+++ Dockernginix.sh
@@ -387,8 +387,8 @@ startInstall()
         echo "    NGinX Proxy Manager admin account."
         echo ""
         echo "    The default login credentials for NGinX Proxy Manager are:"
-        echo "        username: frepai@frepai.com"
-        echo "        password: frepai"
+        echo "        用户名: admin@example.com"
+        echo "        密  码: changeme"
 
         echo ""       
         sleep 3s
